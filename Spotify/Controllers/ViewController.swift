c82718d49ce537b3bc30ecee8abfdaaf13c6a1ee
Evel-Devel <zhenka_nikitin@mail.ru>
2021-03-07 18:53:25 +0700

    Первый коммит учебного проекта

---
+++ Spotify/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Евгений Никитин on 07.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home" 
+    }
+
+
+}
+
