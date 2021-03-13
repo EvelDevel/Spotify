ca238e37e2d0eabeb71fa2e8592e44039d8e54e5
Evel-Devel <zhenka_nikitin@mail.ru>
2021-03-13 18:32:39 +0700

    Второй урок завершил. Авторизация (частичная).

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -8,10 +8,23 @@
 import Foundation
 
 final class AuthManager {
-    
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "33a6a824613b495bb6c17e31ad8e0730"
+        static let clientSecret = "0c835a3c518a4643a92adfcf10bc12e7"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let base = "https://accounts.spotify.com/authorize"
+        let redirectURI = "https://vk.com/ev.nikitin"
+        let dialog = "&show_dialog=TRUE"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)\(dialog)"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -31,5 +44,20 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ) {
+        // Получить токен
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
 }
