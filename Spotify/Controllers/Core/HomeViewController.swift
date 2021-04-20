35a575bc2f0493a397efc61d29bb9795c0fadf06
Evel-Devel <zhenka_nikitin@mail.ru>
2021-04-20 12:29:45 +0700

    Завершил работу над UI главной страницы. Все ячейки сверстаны, контент подгружается.

--- Spotify/Controllers/Core/HomeViewController.swift
+++ Spotify/Controllers/Core/HomeViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 enum BrowseSectionType {
     case newRealeses(viewModels: [NewReleasesCellViewModel])
-    case featuredPlaylists(viewModels: [NewReleasesCellViewModel])
-    case recommendedTracks(viewModels: [NewReleasesCellViewModel])
+    case featuredPlaylists(viewModels: [FeaturedPlaylistCellViewModel])
+    case recommendedTracks(viewModels: [RecommendedTrackCellViewModel])
 }
 
 class HomeViewController: UIViewController {
@@ -159,10 +159,26 @@ class HomeViewController: UIViewController {
                 name: $0.name,
                 artworkURL: URL(string: $0.images.first?.url ?? ""),
                 numberOfTracks: $0.total_tracks,
-                artistName: $0.artists.first?.name ?? "-")
+                artistName: $0.artists.first?.name ?? "-"
+            )
         })))
-        sections.append(.featuredPlaylists(viewModels: []))
-        sections.append(.recommendedTracks(viewModels: []))
+        
+        sections.append(.featuredPlaylists(viewModels: playlists.compactMap({
+            return FeaturedPlaylistCellViewModel(
+                name: $0.name,
+                artworkURL: URL(string: $0.images.first?.url ?? ""),
+                creatorName: $0.owner.display_name
+            )
+        })))
+        
+        sections.append(.recommendedTracks(viewModels: tracks.compactMap({
+            return RecommendedTrackCellViewModel(
+                name: $0.name,
+                artistName: $0.artists.first?.name ?? "-",
+                artworkURL: URL(string: $0.album.images.first?.url ?? "")
+            )
+        })))
+        
         collectionView.reloadData()
     }
     
@@ -348,7 +364,7 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
             ) as? FeaturedPlaylistCollectionViewCell else {
                 return UICollectionViewCell()
             }
-            cell.backgroundColor = .blue
+            cell.configure(with: viewModels[indexPath.row])
             return cell
             
         case .recommendedTracks(let viewModels):
@@ -358,7 +374,7 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
             ) as? RecommendedTrackCollectionViewCell else {
                 return UICollectionViewCell()
             }
-            cell.backgroundColor = .orange
+            cell.configure(with: viewModels[indexPath.row])
             return cell
         }
     }
---
+++ Spotify/ViewModels/FeaturedPlaylistCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FeaturedPlaylistCellViewModel.swift
+//  Spotify
+//
+//  Created by Евгений Никитин on 20.04.2021.
+//
+
+import Foundation
+
+struct FeaturedPlaylistCellViewModel {
+    let name: String
+    let artworkURL: URL?
+    let creatorName: String
+}
